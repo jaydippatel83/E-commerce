ef617a0b28913caf8670166d56d964d825f55394
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-01-13 11:42:22 +0530

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css'; 
-
-class App extends React.Component{
-   constructor(){
-     super()
-     this.state={
-       life:10
-     }
-   } 
-  handleClick=()=>{
-    this.setState({life:this.state.life + 1});
-  }
-  render(){
+import HomePage from './components/HomePage.component';
+ 
+function App(){
   return (
-    <div className="App"> 
-        <p>
-          {this.state.life}
-        </p>
-        <button 
-        onClick={this.handleClick}
-        >
-        Learn React
-        </button> 
+    <div>
+      <HomePage/>
     </div>
   );
- }
-} 
+}
 export default App;
