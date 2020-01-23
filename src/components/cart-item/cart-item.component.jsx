e6f633cd6f834472fe5dd331d2fc2b2d901335c0
Shubhangi Ambade <chirag.jakhariya@rokigroup.co.uk>
2020-01-23 15:25:14 +0530

    add cart

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ReactComponent as ShoppingIcon} from '../../assets/a.svg';
+import './cart-icon.styles.scss';
+
+const CartIcon=()=>(
+    <div className='cart-icon'>
+        <ShoppingIcon className="shopping-icon" />
+        <span className='item-count'>0</span>
+    </div>
+)
+
+export default CartIcon;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,8 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/lg.png';
 import { auth } from '../../firebase/firebase.utils';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import  CartIcon from '../cart-item/cart-item.component';
 
 
 const Header = ({ currentUser }) => (
@@ -21,23 +22,25 @@ const Header = ({ currentUser }) => (
                 contact
             </Link>
             {
-                currentUser ?(
+                currentUser ? (
                     <Link className="option" onClick={() => auth.signOut()}>
                         Sign out
                     </Link>
-                 ) :(
-                    <Link className="option" to="/signin">
-                        Sign In
+                ) : (
+                        <Link className="option" to="/signin">
+                            Sign In
                     </Link>
-                 )
+                    )
             }
+
+            <CartIcon />
 
         </div>
     </div>
 
 );
 
-const mapStateToProps= state=>({
-    currentUser:state.user.currentUser
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
 });
 export default connect(mapStateToProps)(Header);
