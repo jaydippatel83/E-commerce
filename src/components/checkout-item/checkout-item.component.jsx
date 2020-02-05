92adfa14154d12b71571af55a1f3907088218b85
Jaydip patel <jaydippatel83.pj@gmail.com>
2020-02-05 17:02:13 +0530

    confirm dialog

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ClearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import './checkout-item.styles.scss';
 
 
-const CheckoutItem = ({ cartItem, clearItem ,addItem,removeItem}) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
+ 
+    const remove = () => {
+        confirmAlert({
+            title: 'Remove Items',
+            message: 'Are you sure to  remove this item...!  ?',
+            buttons: [
+                {
+                    className: 'btn btn-primary',
+                    label: 'Yes',
+                    onClick: () => clearItem(cartItem)
+                },
+                {
+                    className: 'btn btn-danger',
+                    label: 'No',
+                    onClick: () => clearItem(window.close)
+                }
+            ]
+        });
+    };
+
     return (
         <div className='checkout-item'>
             <div className="image-container">
@@ -13,12 +35,12 @@ const CheckoutItem = ({ cartItem, clearItem ,addItem,removeItem}) => {
             </div>
             <span className="name">{name}</span>
             <span className="quantity">
-                <div className="arrow" onClick={()=>removeItem(cartItem)}>&#10094;</div>
+                <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
                 <span className="value">{quantity}</span>
-                <div className="arrow" onClick={()=> addItem(cartItem)}>&#10095;</div>
+                <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
             <span className="price">{price}</span>
-            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10060;</div>
+            <div className="remove-button" onClick={() => remove()}>&#10060;</div> 
         </div>
     );
 }
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import logo from '../../assets/pay.png';
+import { ToastContainer, toast } from 'react-toastify';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
@@ -8,22 +9,28 @@ const StripeCheckoutButton = ({ price }) => {
 
     const OnToken = token => {
         console.log(token);
-        alert('Payment Successful');
+        toast.success("Payment Successfully....!", {
+            position: toast.POSITION.TOP_RIGHT
+        });
     }
 
     return (
-        <StripeCheckout
-            label='Pay Now'
-            name='CC-Fashion'
-            billingAddress
-            shippingAddress
-            image={logo}
-            description={`Your Total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={OnToken}
-            stripeKey={publishablekey}
-        />
+        <div style={{width:'30%',margin:'10px auto'}}>
+            <ToastContainer />
+            <StripeCheckout
+                style={{width:'100%'}}
+                label='Pay Now'
+                name='CC-Fashion'
+                billingAddress
+                shippingAddress
+                image={logo}
+                description={`Your Total is $${price}`}
+                amount={priceForStripe}
+                panelLabel='Pay Now'
+                token={OnToken}
+                stripeKey={publishablekey}
+            />
+        </div>
     );
 };
 export default StripeCheckoutButton;
