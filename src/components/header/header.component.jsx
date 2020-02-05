8adc12602195a0f77cd21f0081a982a355ae306d
Jaydip patel <jaydippatel83.pj@gmail.com>
2020-02-05 18:15:12 +0530

    headder sticky

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,80 +9,71 @@ import CartDropDown from '../card-dropdown/card-dropdown.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-
+import Headroom from 'react-headroom';
 
 const Header = ({ currentUser, hidden }) => {
 
-    return (
+    return ( 
+        <Headroom style={{ 
+            transition: 'all .5s ease-in-out'
+        }}>
 
 
+            <nav className="navbar navbar-expand-lg navbar-light bg-white  header  ">
 
-        //             currentUser ? (
-        //                 <div className="header-user">
-        //                     <div className="user-image">
-        //                         {/* <img    src={currentUser.photoURL} /> */}
-        //                     </div>
-        //                     <p className="user-name">{currentUser.displayName}</p>
-        //                 </div>
+                <Link className=" navbar-brand logo-container" to="/">
+                    <img src={logo} className="logo" />
+                </Link>
 
-
-
-
-        <nav className="navbar navbar-expand-lg navbar-light bg-white  header">
-
-            <Link className=" navbar-brand logo-container" to="/">
-                <img src={logo} className="logo" />
-            </Link>
-
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto   text-center">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/shop">
-                            shop
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto   text-center">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/shop">
+                                shop
                             </Link>
-                    </li>
-                    <li className="nav-item">
-                        {
-                            currentUser ? (
-                                <Link to="/" className="nav-link" onClick={() => auth.signOut()}>
-                                    Sign out
+                        </li>
+                        <li className="nav-item">
+                            {
+                                currentUser ? (
+                                    <Link to="/" className="nav-link" onClick={() => auth.signOut()}>
+                                        Sign out
                                 </Link>
-                            ) : (
-                                    <Link className="nav-link" to="/signin">
-                                        Sign In
+                                ) : (
+                                        <Link className="nav-link" to="/signin">
+                                            Sign In
                                 </Link>
-                                )
-                        }
-                    </li>
-                    <li className="nav-item  ">
-                        <Link className="nav-link" to="/contact">
-                            contact
+                                    )
+                            }
+                        </li>
+                        <li className="nav-item  ">
+                            <Link className="nav-link" to="/contact">
+                                contact
                         </Link>
-                    </li>
-                    <li className="nav-item active">
-                        <CartIcon className="icon" />
-                    </li>
-                    <li className="nav-item">
-                        {currentUser ? (
-                            <div className="nav-link text-success font-weight-bold">
-                                {/* <div className="user-image"> */}
-                                {/* <img    src={currentUser.photoURL} /> */}
-                                {/* </div> */}
-                                <p className="user-name m-0">{currentUser.displayName}</p>
-                            </div>
+                        </li>
+                        <li className="nav-item active">
+                            <CartIcon className="icon" />
+                        </li>
+                        <li className="nav-item">
+                            {currentUser ? (
+                                <div className="nav-link text-success font-weight-bold">
+                                    {/* <div className="user-image"> */}
+                                    {/* <img    src={currentUser.photoURL} /> */}
+                                    {/* </div> */}
+                                    <p className="user-name m-0">{currentUser.displayName}</p>
+                                </div>
 
-                        ) : ''
+                            ) : ''
 
-                        }
-                    </li>
-                </ul>
-                {hidden ? null : <CartDropDown />}
-            </div>
-        </nav>
-
+                            }
+                        </li>
+                    </ul>
+                    {hidden ? null : <CartDropDown />}
+                </div>
+            </nav>
+        </Headroom>
     );
 }
 
